57e899d5e5aca30b6c27853c0f2890d3e5276e7b
TBS099 <tanushbikram72@gmail.com>
2023-02-17 12:48:33 +0545

    added thumbnail in meta field and meta tag

--- custom-plugin.php
+++ custom-plugin.php
@@ -32,16 +32,20 @@ else {
         //custom metabox field
         public function seo_metabox_field()
         {
-
             $seo_title = get_post_meta(get_the_ID(), 'seo_title', true);
             $seo_description = get_post_meta(get_the_ID(), 'seo_description', true);
+            $seo_thumbnail = wp_get_attachment_image_url(get_post_meta(get_the_ID(), 'seo_attachment_id', true));
             ?>
+            <form method='post' enctype="multipart/form-data">
             SEO Title:<br>
             <input type='text' name='seo-title' value="<?php echo $seo_title; ?>"><br><br>
             SEO Description:<br>
-            <textarea name='seo-description'><?php echo $seo_description; ?></textarea>
+            <textarea name='seo-description'><?php echo $seo_description; ?></textarea><br><br>
+            SEO Thumbnail:<br>
+            <?php echo '<img src="' . $seo_thumbnail . '" alt="user profile image" width="70%" height="auto">';?>
+            <input type='file' accept='image/png, image/jpeg' name='seo_thumbnail' placeholder='Update Thumbnail'><br><br>
+            </form>
             <?php
-
         }
 
         //insert or update seo data
@@ -60,6 +64,19 @@ else {
                 update_post_meta($post->ID, 'seo_description', $_POST["seo-description"]);
 
             endif;
+
+            if (!function_exists('wp_generate_attachment_metadata_2')){
+                require_once(ABSPATH . "wp-admin" . '/includes/image.php');
+                require_once(ABSPATH . "wp-admin" . '/includes/file.php');
+                require_once(ABSPATH . "wp-admin" . '/includes/media.php');
+            }
+            if ($_FILES) {
+                if ($_FILES['seo_thumbnail']['error'] !== UPLOAD_ERR_OK) {
+                    return "upload error : " . $_FILES['seo_thumbnail']['error'];
+                }
+                $attach_id = media_handle_upload( 'seo_thumbnail', get_the_ID() );
+                update_post_meta( get_the_ID(), 'seo_attachment_id', $attach_id );
+            }
         }
 
         //show seo data in header of html page
@@ -69,8 +86,11 @@ else {
 
             $seo_description = get_post_meta($post->ID, 'seo_description', true);
             $seo_title = get_post_meta($post->ID, 'seo_title', true);
+            $seo_thumbnail = wp_get_attachment_image_url(get_post_meta(get_the_ID(), 'seo_attachment_id', true));
+
             echo '<meta name="title" content="' . $seo_title . '">';
             echo '<meta name="description" content="' . $seo_description . '">';
+            echo '<meta name="thumbnail" content="'.$seo_thumbnail.'">';
         }
 
     }
